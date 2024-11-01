873ea60afb4106382448b7bfe98c4007d16c27e2
Rahmad <rajaikhsan2021@gmail.com>
2024-11-01 21:23:55 +0700

    Memindahkan Login dan Main ke package

--- app/src/main/java/com/example/myapplication/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/main/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication
+package com.example.myapplication.ui.main
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -19,6 +19,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.myapplication.R
 
 
 class MainActivity : ComponentActivity() {
